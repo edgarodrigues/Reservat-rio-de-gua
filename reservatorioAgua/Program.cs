e3aa62afaa9813d83ebd60ff42fd30384ccc1576
Edgar Rodrigues <edgarodgui@gmail.com>
2024-10-07 11:13:57 -0300

    Adicionando arquivo

--- reservatorioAgua/Program.cs
+++ reservatorioAgua/Program.cs
@@ -1,42 +1,9 @@
-﻿Console.WriteLine("Determine o objetivo: controle de nível de água: ");
-		if(inteiro.TryParse(Console.ReadLine(), out inteiro controle))
-		{
-			
-			Console.WriteLine("Encher o reservatório de água para o funcionamento da bomba: ");
-			if(inteiro.TryParse(Console.ReadLine(), out inteiro reservatório))
-			{
-			
-		    Console.WriteLine("Escolha as eletroválvulas S1 e S2: ");
-			if(inteiro.TryParse(Console.ReadLine(), out inteiro eletroválvula))
-			{
-				
-				Console.WriteLine("Escolha 3 boias: a,b, e c: ");
-				if(inteiro.TryParse(Console.ReadLine(), out inteiro boias))
-				{
-					
-					
-						
-				   Console.WriteLine("Ligar a bomba quando o nível de água do reservatório estiver abaixo da boia c: ");
-				   if(inteiro.TryParse(Console.ReadLine(), out inteiro bomba))
-				   {
-							
-						Console.WriteLine("Acionar a boia b: ");
-						if(inteiro.TryParse(Console.ReadLine(), out inteiro boia b))
-						{
-							
-						    Console.WriteLine("Permita a entrada de água na eletroválvula S2: ");
-						    if(inteiro.TryParseConsole.ReadLine(), out inteiro eletroválvula S2))
-						    {
-							
-						    }
-							
-						    else
-							
-						    {
-							
-						           Console.WriteLine("erro:inconsistência no monitoramento do reservatório 1:bomba 
-				               e eletroválvula não poderão ser acionadas: ");
-													 
-		
-        
-			       }
+﻿Console.WriteLine("Controle caixa d'água");
+Console.Write("Informar o status da bomba A: ");
+int bomba_A = int.Parse(Console.ReadLine());
+
+Console.Write("Informar o status da bomba B: ");
+int bomba_B = int.Parse(Console.ReadLine());
+
+Console.Write("Informar o status da bomba C: ");
+int bomba_C = int.Parse(Console.ReadLine());
